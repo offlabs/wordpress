f621636eb84a43fe4afb92b68a3b8185e0e9f648
nathanielelliott <n.elliott@nathanielelliott.com>
2014-05-08 09:05:38 -0400

    Base Files

---
+++ footer.php
@@ -0,0 +1,11 @@
+<div id="footer">
+    <p>
+    <strong>Copyright 2011 <?php bloginfo('name'); ?> | All Rights Reserved.</strong> </a> 
+    Designed by <a href="http://slackrmedia.com">SlackrMedia</a>
+    </p>
+    <p><a href="<?php bloginfo('rss2_url'); ?>">Latest Stories RSS</a> | <a href="<?php comments_rss_link('comment feed'); ?>">Comments RSS</a></p>
+    </div>
+</div>
+ 
+</body>
+</html>
