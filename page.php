39ed90425866f079a83069cb69cbb0f9c899812c
nathanielelliott <n.elliott@nathanielelliott.com>
2014-05-10 23:37:34 -0400

    extra display:none set

--- page.php
+++ page.php
@@ -1,32 +1,23 @@
 <?php get_header(); ?>
  
-    <div id="blog">
-        <?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
+    <?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
          
-        <div class="post">
         <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
  
- 
-            <div class="entry">
             <?php the_content(); ?>
  
-                <p class="postmetadata">
+                <p class="postmetadata" style="display:none;">
                 <?php _e('Filed under&#58;'); ?> <?php the_category(', ') ?> <?php _e('by'); ?> <?php  the_author(); ?><br />
                 <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?> <?php edit_post_link('Edit', ' &#124; ', ''); ?>
                 </p>
- 
-            </div>
- 
-        </div>
          
 <?php endwhile; ?>
  
-    <div class="navigation">
+    <div class="navigation" style="display:none;">
         <?php posts_nav_link(); ?>
     </div>
  
 <?php endif; ?>
-</div>
  
-<?php get_sidebar(); ?>   
+<!--<?php get_sidebar(); ?>   -->
 <?php get_footer(); ?>
