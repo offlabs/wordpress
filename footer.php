0ea0dcd38f58484d067534ab7e6f0185d7899714
nathanielelliott <n.elliott@nathanielelliott.com>
2014-05-15 09:59:00 -0400

    Error in JS links

--- footer.php
+++ footer.php
@@ -6,8 +6,8 @@
       </div>
     </div>
  
-	<script src="<?php bloginfo( 'template_url') ?>js/jquery.min.js"></script>
-	<script src="<?php bloginfo( 'template_url') ?>js/bootstrap.min.js"></script>
-	<script src="<?php bloginfo( 'template_url') ?>js/main.js"></script>
+	<script src="<?php bloginfo( 'template_url') ?>/js/jquery.min.js"></script>
+	<script src="<?php bloginfo( 'template_url') ?>/js/bootstrap.min.js"></script>
+	<script src="<?php bloginfo( 'template_url') ?>/js/main.js"></script>
 </body>
 </html>
