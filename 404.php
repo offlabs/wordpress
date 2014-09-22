1460c70b1ec8cddbb67c38a4902c2a836c5ccbd1
nathanielelliott <nathaniel.p.elliott@gmail.com>
2014-09-22 19:03:23 -0400

    Adding 404 page

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+		
+		<style>
+		img {margin-top:100px;}
+		.drop {margin-bottom:25px;}
+		span {font-size: 6px;}
+		</style>
+        
+        <img src="<?php echo get_bloginfo('template_directory');?>/img/404.gif" alt="Smiley Time" height="177" width="276">
+        <p><span>Image Credits: &copy; Reaction Gifs - http://www.reactiongifs.com/start-bawling/</span></p>
+        <h1>404</h1>
+        <p class="drop">You may have hit the wrong button!</p>
+        <a class="btn btn-primary drop" href="/" type="button">Go back?</a>
+ 
+<?php get_footer(); ?>
